d29cfb0dcbbb3e4145e01b0af603081e270e7e54
Jiri Malek <jirka.malek@gmail.com>
2020-03-27 19:43:00 +0100

    Add common shared libs reference.

---
+++ src/common.h
@@ -0,0 +1,16 @@
+// general C libs
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <string.h>
+
+// Ledger operating system related stuff
+#include <os.h>
+#include <os_io_seproxyhal.h>
+
+// our common libs
+#include "utils.h"
+#include "assert.h"
+#include "io.h"
+#include "ux.h"
+#include "errors.h"
