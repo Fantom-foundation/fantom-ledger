fc8d40e9b81ea2aa939633f140389074a6acddf8
Jiri Malek <jirka.malek@gmail.com>
2020-03-27 19:42:43 +0100

    Add assertion enforcement macros and implementation.

---
+++ src/assert.c
@@ -0,0 +1,33 @@
+#include "common.h"
+#include "assert.h"
+
+// assert implements the assertion test
+// on PRODUCTION mode it will call Reset on the device
+// on DEBUG mode we print the assertion and continue with the
+// flow since it takes several loops of SEPROXYHAL to render on display.
+void assert(
+        int cond,
+        const char *msgStr
+#ifdef RESET_ON_CRASH
+        MARK_UNUSED
+#endif
+) {
+    // condition is met, nothing to do
+    if (cond) return;
+
+#ifdef RESET_ON_CRASH
+    io_seproxyhal_se_reset();
+#else
+    {
+#ifdef DEVEL
+        {
+            PRINTF("Assertion failed %s\n", msgStr);
+            ui_displayPaginatedText("Assertion failed", msgStr, NULL);
+            THROW(ERR_ASSERT);
+        }
+#else
+#			error "RESET_ON_CRASH should be enabled in non-devel mode!"
+#endif
+    }
+#endif
+}
---
+++ src/assert.h
@@ -0,0 +1,26 @@
+#ifndef FANTOM_LEDGER_ASSERT_H
+#define FANTOM_LEDGER_ASSERT_H
+
+// convenience redefinition of c keyword
+#define STATIC_ASSERT _Static_assert
+
+// the actual function we use for assertion
+extern void assert(int cond, const char *msgStr);
+
+// substitutes function above, but it's more eye catching
+#define ASSERT_WITH_MSG(cond, msg) assert(cond, msg)
+
+// max length of an assert we allow
+#define _MAX_ASSERT_LENGTH_ 25
+
+// make sure we don't show more than _MAX_ASSERT_LENGTH_ chars; if so cat the beginning and show the tail
+#define _ASSERT_MSG_LEN_(strLiteral, size) (sizeof(strLiteral) > size ? (strLiteral) + sizeof(strLiteral) - size : strLiteral)
+
+// from https://stackoverflow.com/questions/19343205/c-concatenating-file-and-line-macros
+#define _TO_STR1_(x) #x
+#define _TO_STR2_(x) _TO_STR1_(x)
+#define _FILE_LINE_ __FILE__ ":" _TO_STR2_(__LINE__)
+
+// the basic ASSERT will work with file line
+#define ASSERT(cond) assert((cond), _ASSERT_MSG_LEN_(_FILE_LINE_, _MAX_ASSERT_LENGTH_))
+#endif //FANTOM_LEDGER_ASSERT_H
