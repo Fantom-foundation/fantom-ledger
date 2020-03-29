7b29108654765bac15cfd087fc902dfece108f85
Jiri Malek <jirka.malek@gmail.com>
2020-03-29 15:04:08 +0200

    Add missing error for invalid INS params.

--- src/errors.h
+++ src/errors.h
@@ -3,31 +3,44 @@
 
 enum {
     // Instruction processed
-    SUCCESS                        = 0x9000,
+    SUCCESS = 0x9000,
 
-    // Bad request header
-    ERR_BAD_REQUEST_HEADER         = 0x6E01,
+    // Error codes below this value are ok to be passed to host.
+    // We don't want to pass unknown error though, so any other error code will
+    // be handled by resetting the device.
+    // Note that any error will reset multi-step instruction processing.
+    _ERR_PASS_FROM = 0x6E00,
 
-    // Unknown CLA
-    ERR_UNKNOWN_CLA                = 0x6E02,
+    // Bad request header.
+    ERR_BAD_REQUEST_HEADER = 0x6E01,
 
-    // Unknown INS
-    ERR_UNKNOWN_INS                = 0x6E03,
+    // Unknown service class CLA received.
+    ERR_UNKNOWN_CLA = 0x6E02,
 
-    // Request is not valid in the current context
-    ERR_INVALID_STATE              = 0x6E04,
+    // Unknown instruction arrived.
+    ERR_UNKNOWN_INS = 0x6E03,
 
-    // Request contains invalid P1, P2, or Data
-    ERR_INVALID_DATA               = 0x6E05,
+    // Request is not valid in the current context.
+    ERR_INVALID_STATE = 0x6E04,
 
-    // Action has been rejected by user
-    ERR_REJECTED_BY_USER           = 0x6E06,
+    // Request contains invalid parameters P1, P2, or Lc.
+    ERR_INVALID_PARAMETERS = 0x6E05,
 
-    // Action rejected by security policy
-    ERR_REJECTED_BY_POLICY         = 0x6E07,
+    // Request contains invalid payload structure, or content.
+    ERR_INVALID_DATA = 0x6E06,
 
-    // Device is locked
-    ERR_DEVICE_LOCKED              = 0x6E08,
+    // Action has been rejected by user.
+    ERR_REJECTED_BY_USER = 0x6E07,
+
+    // Action rejected by security policy.
+    ERR_REJECTED_BY_POLICY = 0x6E08,
+
+    // Device is locked.
+    ERR_DEVICE_LOCKED = 0x6E09,
+
+    // Error codes above this value are ok to be passed to host.
+    // Any other error will trigger SEPROXYHAL reset.
+    _ERR_PASS_TO = 0x6E10
 };
 
 #endif
