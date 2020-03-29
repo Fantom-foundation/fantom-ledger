d6c2d2f29dbd50850d6e9d8c46e5c15bd54dca8d
Jiri Malek <jirka.malek@gmail.com>
2020-03-29 15:44:52 +0200

    Add utility and base UX macros.

---
+++ src/utils.h
@@ -0,0 +1,50 @@
+#ifndef FANTOM_LEDGER_UTILS_H
+#define FANTOM_LEDGER_UTILS_H
+
+#include <os.h>
+#include "assert.h"
+
+// Does not compile if x is pointer of some kind.
+// See http://zubplot.blogspot.com/2015/01/gcc-is-wonderful-better-arraysize-macro.html
+#define ARRAY_NOT_A_PTR(x) \
+    (sizeof(__typeof__(int[1 - 2 * \
+        !!__builtin_types_compatible_p(__typeof__(x), \
+        __typeof__(&x[0]))])) * 0)
+
+
+// Safe array length, does not compile if you accidentally supply a pointer.
+#define ARRAY_LEN(arr) (sizeof(arr) / sizeof((arr)[0]) + ARRAY_NOT_A_PTR(arr))
+
+// Does not compile if x *might* be a pointer of some kind.
+// Might produce false positives on small structs.
+// Note: ARRAY_NOT_A_PTR does not compile if arg is a struct so this is a workaround
+#define SIZEOF_NOT_A_PTR(var) (sizeof(__typeof(int[0 - (sizeof(var) == sizeof((void *)0))])) * 0)
+
+// Safe version of SIZEOF, does not compile if you accidentally supply a pointer.
+#define SIZEOF(var) (sizeof(var) + SIZEOF_NOT_A_PTR(var))
+
+// Validate the expression type.
+#define ASSERT_TYPE(expr, expected_type) \
+    STATIC_ASSERT( __builtin_types_compatible_p(__typeof__((expr)), expected_type), "Wrong type")
+
+// Given that memset is root of many problems, a bit of paranoia is good.
+#define MEMCLEAR(ptr, expected_type) \
+    do { \
+        STATIC_ASSERT(sizeof(expected_type) == sizeof(*(ptr)), "bad memclear parameters"); \
+        os_memset(ptr, 0, sizeof(expected_type)); \
+    } while(0)
+
+// Helper macro to check APDU request parameters.
+#define VALIDATE(cond, error) \
+    do {\
+        if (!(cond)) { \
+            THROW(error); \
+        } \
+    } while(0)
+
+// Note: unused removes unused warning but does not warn if you suddenly
+// start using such variable. deprecated deals with that.
+#define MARK_UNUSED __attribute__ ((unused, deprecated))
+
+
+#endif //FANTOM_LEDGER_UTILS_H
