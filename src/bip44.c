450c32bf5aa8454da308a95ca43184d803f090be
Jiri Malek <jirka.malek@gmail.com>
2020-04-02 22:06:46 +0200

    Add BIP44 path handing implementation.

---
+++ src/bip44.c
@@ -0,0 +1,148 @@
+#include "common.h"
+#include "errors.h"
+#include "bip44.h"
+#include "endian.h"
+
+// MAX_REASONABLE_ACCOUNT is maximal number of accounts managed by the ledger app (index 2)
+static const uint32_t MAX_REASONABLE_ACCOUNT = 10;
+
+// OPERA_CHANGE_INTERNAL is index of internal Opera chain (internal change in terms of BIP44 specs)
+static const uint32_t OPERA_CHANGE_INTERNAL = 1;
+
+// OPERA_CHANGE_EXTERNAL is index of public Opera chain (external change in terms of BIP44 specs)
+static const uint32_t OPERA_CHANGE_EXTERNAL = 0;
+
+// MAX_REASONABLE_ADDRESS is the maximal number of addresses derived for single account (index 4)
+static const uint32_t MAX_REASONABLE_ADDRESS = 1000000;
+
+// bip44_parseFromWire implements parsing of BIP44 path from incoming APDU buffer.
+size_t bip44_parseFromWire(
+        bip44_path_t *path,
+        const uint8_t *dataBuffer, size_t dataSize
+) {
+    // make sure the buffer length make sense
+    VALIDATE(dataSize >= 1, ERR_INVALID_DATA);
+
+    // get the number of BIP32 path elements signaled to be in the buffer
+    // see INS specification for incoming data payload ([0] = Number of BIP32 Derivations)
+    size_t length = dataBuffer[0];
+
+    // make sure the number of BIP32 derivations corresponds with expected path length
+    VALIDATE(length <= ARRAY_LEN(path->path), ERR_INVALID_DATA);
+
+    // make sure the buffer size is in pair with the announced BIP32 derivations
+    // each derivation is 4 bytes long, the first byte is the number of derivations included
+    VALIDATE(length * 4 + 1 <= dataSize, ERR_INVALID_DATA);
+
+    // keep the number of derivations
+    path->length = length;
+
+    // loop to extract derivations out of the buffer to the path
+    size_t offset = 1;
+    for (size_t i = 0; i < length; i++) {
+        path->path[i] = u4be_read(dataBuffer + offset);
+        offset += 4;
+    }
+
+    return offset;
+}
+
+// bip44_isHardened implements check if the given index value is hardened.
+bool bip44_isHardened(uint32_t value) {
+    return value == (value | HARDENED_BIP32);
+}
+
+// bip44_hasValidFantomPrefix implements test for valid BIP44 prefix in the BIP44 path
+// for Fantom address derivation.
+bool bip44_hasValidFantomPrefix(const bip44_path_t *path) {
+#define CHECK(cond) if (!(cond)) return false
+    // we should have at least the account number
+    CHECK(path->length > BIP44_I_COIN_TYPE);
+
+    // the purpose should be #44 (BIP44 specification) and should be hardened
+    CHECK(path->path[BIP44_I_PURPOSE] == (BIP_44 | HARDENED_BIP32));
+
+    // the coin type has to be valid Fantom coin type and should be hardened
+    CHECK(path->path[BIP44_I_COIN_TYPE] == (FANTOM_COIN_TYPE | HARDENED_BIP32));
+    return true;
+#undef CHECK
+}
+
+// bip44_containsAccount implements check if the BIP44 path contains account number (index 2).
+bool bip44_containsAccount(const bip44_path_t *path) {
+    return path->length > BIP44_I_ACCOUNT;
+}
+
+// bip44_getAccount returns the account index of the BIP44 path (index 2).
+uint32_t bip44_getAccount(const bip44_path_t *path) {
+    ASSERT(path->length > BIP44_I_ACCOUNT);
+    return path->path[BIP44_I_ACCOUNT];
+}
+
+// bip44_hasReasonableAccount implements check if the BIP44
+// path contains reasonable account number (index 2).
+bool bip44_hasReasonableAccount(const bip44_path_t *path) {
+    // account must be present
+    if (!bip44_containsAccount(path)) return false;
+
+    // account index must be hardened
+    uint32_t account = bip44_getAccount(path);
+    if (!bip44_isHardened(account)) return false;
+
+    // remove hardening from the account index
+    // so we can compare it to the max reasonable account
+    account = account & (~HARDENED_BIP32);
+    return account < MAX_REASONABLE_ACCOUNT;
+}
+
+// bip44_containsChangeType implements check if the BIP44 path includes chain type (index 3).
+bool bip44_containsChangeType(const bip44_path_t *path) {
+    return path->length > BIP44_I_CHANGE;
+}
+
+// bip44_getChangeType returns the chain type value of the BIP44 path (index 3).
+bool bip44_getChangeType(const bip44_path_t *path) {
+    // must have the change / chain index
+    ASSERT(path->length > BIP44_I_CHANGE);
+    return path->path[BIP44_I_CHANGE];
+}
+
+// bip44_hasValidChangeType implements check for valid chain type in the BIP44 path (index 3).
+bool bip44_hasValidChangeType(const bip44_path_t *path) {
+    // change type is present in the path
+    if (!bip44_containsChangeType(path)) return false;
+
+    // get the change type value and make sure it is valid
+    uint32_t type = bip44_getChangeType(path);
+    return (type == OPERA_CHANGE_EXTERNAL) || (type == OPERA_CHANGE_INTERNAL);
+}
+
+// bip44_containsAddress implements check if the BIP44 path
+// includes address index within the account (index 4)
+bool bip44_containsAddress(const bip44_path_t *path) {
+    return path->length > BIP44_I_ADDRESS;
+}
+
+// bip44_getAddress returns the address index of the BIP44 path.
+uint32_t bip44_getAddress(const bip44_path_t *path) {
+    // must have the address index
+    ASSERT(path->length > BIP44_I_ADDRESS);
+    return path->path[BIP44_I_ADDRESS];
+}
+
+// bip44_hasReasonableAddress implements check if the BIP44 path
+// includes reasonable address index within the account (index 4)
+bool bip44_hasReasonableAddress(const bip44_path_t *path) {
+    // the address has to be present
+    if (!bip44_containsAddress(path)) return false;
+
+    // get the address index and compare it to the reasonable limit
+    uint32_t address = bip44_getAddress(path);
+    return (address <= MAX_REASONABLE_ADDRESS);
+}
+
+// bip44_containsMoreThanAddress implements check if the BIP44 path contains
+// indexes beyond address index (index 5+)
+bool bip44_containsMoreThanAddress(const bip44_path_t *path) {
+    return (path->length > BIP44_I_REST);
+}
---
+++ src/bip44.h
@@ -0,0 +1,72 @@
+#ifndef FANTOM_LEDGER_BIP44_H
+#define FANTOM_LEDGER_BIP44_H
+
+#include "common.h"
+#include "conf.h"
+
+// bip44_path_t declares full BIP44 path type
+// see BIP44 specification https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
+typedef struct {
+    uint32_t path[BIP44_MAX_PATH_LENGTH];
+    uint32_t length;
+} bip44_path_t;
+
+// BIP_44 defines BIP44 purpose index value (path index 0)
+static const uint32_t BIP44 = 44;
+
+// FANTOM_COIN_TYPE defines BIP44 coin type for Fantom/Ether (path index 1).
+// This may need to be changed in the future if Fantom will opt for their own registered coin type.
+static const uint32_t FANTOM_COIN_TYPE = 60;
+
+// HARDENED_BIP32 defines a mask for hardened path index (apostrophe in the path value)
+static const uint32_t HARDENED_BIP32 = ((uint32_t) 1 << 31);
+
+// Specific indexes of elements of the BIP44 path
+enum {
+    BIP44_I_PURPOSE = 0,
+    BIP44_I_COIN_TYPE = 1,
+    BIP44_I_ACCOUNT = 2,
+    BIP44_I_CHANGE = 3,
+    BIP44_I_ADDRESS = 4,
+    BIP44_I_REST = 5,
+};
+
+// bip44_parseFromWire implements parsing of BIP44 path from incoming APDU buffer.
+size_t bip44_parseFromWire(
+        bip44_path_t *path,
+        const uint8_t *dataBuffer, size_t dataSize
+);
+
+// bip44_hasValidFantomPrefix implements test for valid BIP44 prefix in the BIP44 path
+// for Fantom address derivation.
+bool bip44_hasValidFantomPrefix(const bip44_path_t *path);
+
+// bip44_containsAccount implements check if the BIP44 path contains account number (index 2).
+bool bip44_containsAccount(const bip44_path_t *path);
+
+// bip44_hasReasonableAccount implements check if the BIP44
+// path contains reasonable account number (index 2).
+bool bip44_hasReasonableAccount(const bip44_path_t *path);
+
+// bip44_containsChangeType implements check if the BIP44 path includes chain type (index 3).
+bool bip44_containsChangeType(const bip44_path_t *path);
+
+// bip44_hasValidChangeType implements check for valid chain type in the BIP44 path (index 3).
+bool bip44_hasValidChangeType(const bip44_path_t *path);
+
+// bip44_containsAddress implements check if the BIP44 path
+// includes address index within the account (index 4)
+bool bip44_containsAddress(const bip44_path_t *path);
+
+// bip44_hasReasonableAddress implements check if the BIP44 path
+// includes reasonable address index within the account (index 4)
+bool bip44_hasReasonableAddress(const bip44_path_t *path);
+
+// bip44_containsMoreThanAddress implements check if the BIP44 path contains
+// indexes beyond address index (index 5+)
+bool bip44_containsMoreThanAddress(const bip44_path_t *path);
+
+// bip44_isHardened implements check if the given index value is hardened.
+bool bip44_isHardened(uint32_t value);
+
+#endif //FANTOM_LEDGER_BIP44_H
