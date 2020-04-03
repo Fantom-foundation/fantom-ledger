fbe3a622cd95373eb91b26970b73a92ff5d72697
Jiri Malek <jirka.malek@gmail.com>
2020-04-03 20:22:20 +0200

    Add instruction state for Get Public Key.

--- src/state.c
+++ src/state.c
@@ -3,3 +3,9 @@
 // currentIns holds the current instruction being processed.
 // We do not allow instruction switching in the middle for a processing chain.
 int currentIns;
+
+// instructionState holds the current APDU instruction context
+// and state.
+// We use joined structure since only one instruction can run
+// at any time.
+instruction_state_t instructionState;
--- src/state.h
+++ src/state.h
@@ -1,12 +1,21 @@
 #ifndef FANTOM_LEDGER_STATE_H
 #define FANTOM_LEDGER_STATE_H
 
+#include "get_pub_key.h"
+
 // Declares what instructions are recognized and processed by the application.
 #define INS_NONE = -1;
 #define INS_VERSION = 0x01;
 #define INS_GET_KEY = 0x10;
 #define INS_GET_ADDR = 0x11;
 #define INS_SIGN_TX = 0x20;
+
+// instruction_state_t defines unified APDU instruction state.
+// We use joined instruction state storage since only one instruction
+// can rut at any time.
+typedef union {
+    ins_get_ext_pubkey_context_t insGetPubKeyContext;
+} instruction_state_t;
 
 // currentIns declares a current instruction registry.
 // Instructions received from APDU are uint8_t, but we have a special INS_NONE value
