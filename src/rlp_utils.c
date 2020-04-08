3043f611f6ccee7ce542f79cd70955b41e49dfe1
Jiri Malek <jirka.malek@gmail.com>
2020-04-08 19:25:20 +0200

    Remove obsolete "os.h" and "cx.h" dependencies.

--- src/rlp_utils.c
+++ src/rlp_utils.c
@@ -12,8 +12,6 @@
 #include <stdint.h>
 #include <string.h>
 
-#include "os.h"
-#include "cx.h"
 #include "common.h"
 #include "utils.h"
 #include "errors.h"
@@ -75,7 +73,7 @@ bool rlpCanDecode(uint8_t *buffer, uint32_t length, bool *isValid) {
 // some details about the field itself.
 bool rlpDecodeLength(uint8_t *buffer, uint32_t bufferLength, uint32_t *fieldLength, uint32_t *offset, bool *isList) {
     // sanity check, we need to have at least single byte in the buffer
-    VALIDATE(length > 0, ERR_INVALID_DATA);
+    VALIDATE(bufferLength > 0, ERR_INVALID_DATA);
 
     // decide what to do based on the buffer content
     if (*buffer <= 0x7f) {
@@ -96,28 +94,28 @@ bool rlpDecodeLength(uint8_t *buffer, uint32_t bufferLength, uint32_t *fieldLeng
         switch (*buffer) {
             case 0xb8:
                 // sanity check, we need to have at least two bytes in the buffer
-                VALIDATE(length > 1, ERR_INVALID_DATA);
+                VALIDATE(bufferLength > 1, ERR_INVALID_DATA);
 
                 // get the length
                 *fieldLength = *(buffer + 1);
                 break;
             case 0xb9:
                 // sanity check, we need to have at least three bytes in the buffer
-                VALIDATE(length > 2, ERR_INVALID_DATA);
+                VALIDATE(bufferLength > 2, ERR_INVALID_DATA);
 
                 // get the length
                 *fieldLength = (*(buffer + 1) << 8) + *(buffer + 2);
                 break;
             case 0xba:
                 // sanity check, we need to have at least four bytes in the buffer
-                VALIDATE(length > 3, ERR_INVALID_DATA);
+                VALIDATE(bufferLength > 3, ERR_INVALID_DATA);
 
                 // get the length
                 *fieldLength = (*(buffer + 1) << 16) + (*(buffer + 2) << 8) + *(buffer + 3);
                 break;
             case 0xbb:
                 // sanity check, we need to have at least five bytes in the buffer
-                VALIDATE(length > 4, ERR_INVALID_DATA);
+                VALIDATE(bufferLength > 4, ERR_INVALID_DATA);
 
                 // get the length
                 *fieldLength = (*(buffer + 1) << 24) + (*(buffer + 2) << 16) +
@@ -140,28 +138,28 @@ bool rlpDecodeLength(uint8_t *buffer, uint32_t bufferLength, uint32_t *fieldLeng
         switch (*buffer) {
             case 0xf8:
                 // sanity check, we need to have at least two bytes in the buffer
-                VALIDATE(length > 1, ERR_INVALID_DATA);
+                VALIDATE(bufferLength > 1, ERR_INVALID_DATA);
 
                 // get the length
                 *fieldLength = *(buffer + 1);
                 break;
             case 0xf9:
                 // sanity check, we need to have at least three bytes in the buffer
-                VALIDATE(length > 2, ERR_INVALID_DATA);
+                VALIDATE(bufferLength > 2, ERR_INVALID_DATA);
 
                 // get the length
                 *fieldLength = (*(buffer + 1) << 8) + *(buffer + 2);
                 break;
             case 0xfa:
                 // sanity check, we need to have at least four bytes in the buffer
-                VALIDATE(length > 3, ERR_INVALID_DATA);
+                VALIDATE(bufferLength > 3, ERR_INVALID_DATA);
 
                 // get the length
                 *fieldLength = (*(buffer + 1) << 16) + (*(buffer + 2) << 8) + *(buffer + 3);
                 break;
             case 0xfb:
                 // sanity check, we need to have at least five bytes in the buffer
-                VALIDATE(length > 4, ERR_INVALID_DATA);
+                VALIDATE(bufferLength > 4, ERR_INVALID_DATA);
 
                 // get the length
                 *fieldLength = (*(buffer + 1) << 24) + (*(buffer + 2) << 16) +
